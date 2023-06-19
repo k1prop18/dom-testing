e90d32e97e4e8fcd9e9b3d2ad85063b57024f1be
Caleb Kiprop Rono <ckiprop14@gmail.com>
2023-06-19 19:30:46 +0300

    Assignment done

---
+++ my-portfolio/script.js
@@ -0,0 +1,4 @@
+onst h2 = document.createElement("h2");
+h2.textContent = "This content added by Javascript";
+document.querySelector("bosy").appendChild(h2);
+/home/caleb/Moringa/javascript-events-acting-on-events-lab
